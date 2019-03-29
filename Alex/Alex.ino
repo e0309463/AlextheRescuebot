3a745cb670a53b12931589b18588b25efdd054bc
Hong Chuan <hongchuan.nus@gmail.com>
2019-03-29 16:47:00 +0800

    bare-metal PWM

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -2,6 +2,7 @@
 
 #include "packet.h"
 #include "constants.h"
+
 typedef enum
 {
   STOP = 0,
@@ -29,10 +30,10 @@ volatile TDirection dir = STOP;
 
 // Motor control pins. You need to adjust these till
 // Alex moves in the correct direction
-#define LF                  6   // Left forward pin
-#define LR                  5   // Left reverse pin
-#define RF                  10  // Right forward pin
-#define RR                  11  // Right reverse pin
+#define LF                  0b00100000   // Left forward pin 6 port d
+#define LR                  0b00010000   // Left reverse pin 5 port d
+#define RF                  0b00000010  // Right forward pin 10 port b
+#define RR                  0b00000001  // Right reverse pin 9 port b
 
 // Pi, for calculating turn circumference 
 #define PI 3.141592654
@@ -66,9 +67,13 @@ volatile unsigned long forwardDist;
 volatile unsigned long reverseDist;
 
 //Variables to keep track of whether we have moved a commanded distance
-unsigned long deltaDist;
-unsigned long newDist;
+volatile unsigned long deltaDist;
+volatile unsigned long newDist;
 
+static volatile int LFval;
+static volatile int LRval;
+static volatile int RFval;
+static volatile int RRval;
 /*
  * 
  * Alex Communication Routines.
@@ -268,6 +273,7 @@ void setupEINT()
 // Implement the external interrupt ISRs below.
 // INT0 ISR should call leftISR while INT1 ISR
 // should call rightISR.
+// Implement INT0 and INT1 ISRs above.
 
 ISR(INT0_vect)
 {
@@ -279,8 +285,162 @@ ISR(INT1_vect)
   rightISR();
 }
 
+ISR(TIMER0_COMPA_vect)
+{
+OCR0A = LFval;
+}
 
-// Implement INT0 and INT1 ISRs above.
+ISR(TIMER0_COMPB_vect)
+{
+OCR0B = LRval;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+OCR1A = RRval;
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+OCR1B = RFval;
+}
+
+int pwmVal(float speed)
+{
+  if(speed < 0.0)
+    speed = 0;
+
+  if(speed > 100.0)
+    speed = 100.0;
+
+  return (int) ( (speed / 100.0) * 255.0 );
+}
+
+// Move Alex forward "dist" cm at speed "speed".
+// "speed" is expressed as a percentage. E.g. 50 is
+// move forward at half speed.
+// Specifying a distance of 0 means Alex will
+// continue moving forward indefinitely.
+void forward(float dist, float speed)
+{
+  if(dist > 0)
+    deltaDist = dist;
+  else
+    deltaDist = 9999999;
+
+  newDist = forwardDist + deltaDist;
+  
+  dir = FORWARD;
+  int val = pwmVal(speed);
+
+  // For now we will ignore dist and move
+  // forward indefinitely. We will fix this
+  // in Week 9.
+
+  // LF = Left forward pin, LR = Left reverse pin
+  // RF = Right forward pin, RR = Right reverse pin
+  // This will be replaced later with bare-metal code.
+  TCCR0A = 0b10000001;
+  PORTD &= ~LR; //off LR
+  LFval = val;
+
+  TCCR1A = 0b00100001;
+  PORTB &= ~RR; //off RR
+  RFval = val;
+  
+}
+
+// Reverse Alex "dist" cm at speed "speed".
+// "speed" is expressed as a percentage. E.g. 50 is
+// reverse at half speed.
+// Specifying a distance of 0 means Alex will
+// continue reversing indefinitely.
+void reverse(float dist, float speed)
+{
+  if(dist > 0)
+    deltaDist = dist;
+  else
+    deltaDist = 9999999;
+
+  newDist = reverseDist + deltaDist;
+  dir = BACKWARD;
+  int val = pwmVal(speed);
+
+  // For now we will ignore dist and 
+  // reverse indefinitely. We will fix this
+  // in Week 9.
+
+  // LF = Left forward pin, LR = Left reverse pin
+  // RF = Right forward pin, RR = Right reverse pin
+  // This will be replaced later with bare-metal code.
+  TCCR0A = 0b00100001;
+  PORTD &= ~LF; //off LF
+  LRval = val;
+
+  TCCR1A = 0b10000001;
+  PORTB &= ~RF; //off RF
+  RRval = val;
+}
+
+// Turn Alex left "ang" degrees at speed "speed".
+// "speed" is expressed as a percentage. E.g. 50 is
+// turn left at half speed.
+// Specifying an angle of 0 degrees will cause Alex to
+// turn left indefinitely.
+void left(float ang, float speed)
+{
+  dir = LEFT;
+  int val = pwmVal(speed);
+
+  // For now we will ignore ang. We will fix this in Week 9.
+  // We will also replace this code with bare-metal later.
+  // To turn left we reverse the left wheel and move
+  // the right wheel forward.
+  TCCR0A = 0b00100001;
+  PORTD &= ~LF; //off LF
+  LRval = val;
+
+  TCCR1A = 0b00100001;
+  PORTB &= ~RR; //off RR
+  RFval = val;
+
+}
+
+// Turn Alex right "ang" degrees at speed "speed".
+// "speed" is expressed as a percentage. E.g. 50 is
+// turn left at half speed.
+// Specifying an angle of 0 degrees will cause Alex to
+// turn right indefinitely.
+void right(float ang, float speed)
+{
+  dir = RIGHT;
+  int val = pwmVal(speed);
+
+  // For now we will ignore ang. We will fix this in Week 9.
+  // We will also replace this code with bare-metal later.
+  // To turn right we reverse the right wheel and move
+  // the left wheel forward.
+  TCCR0A = 0b10000001;
+  PORTD &= ~LR; //off LR
+  LFval = val;
+
+  TCCR1A = 0b10000001;
+  PORTB &= ~RF; //off RF
+  RRval = val;
+
+}
+
+// Stop Alex. To replace with bare-metal code later.
+void stop()
+{
+  dir = STOP;
+  TCCR0A = 0b00000001;
+  PORTD &= (~LR & ~LF); //off LR & LF
+
+  TCCR1A = 0b00000001;
+  PORTB &= ( ~RR & ~ RF ); //off RF & RF
+
+}
 
 /*
  * Setup and start codes for serial communications
@@ -329,23 +489,16 @@ void writeSerial(const char *buffer, int len)
   Serial.write(buffer, len);
 }
 
+
+
 /*
- * Alex's motor drivers.
+ * Alex's setup and run codes
  * 
  */
-
-// Set up Alex's motors. Right now this is empty, but
+ // Set up Alex's motors. Right now this is empty, but
 // later you will replace it with code to set up the PWMs
 // to drive the motors.
-void setupMotors()
-{
-  /* Our motor set up is:  
-   *    A1IN - Pin 5, PD5, OC0B
-   *    A2IN - Pin 6, PD6, OC0A
-   *    B1IN - Pin 10, PB2, OC1B
-   *    B2In - pIN 11, PB3, OC2A
-   */
-}
+
 
 // Start the PWM for Alex's motors.
 // We will implement this later. For now it is
@@ -354,133 +507,27 @@ void startMotors()
 {
   
 }
-
-// Convert percentages to PWM values
-int pwmVal(float speed)
+ void setupMotors()
 {
-  if(speed < 0.0)
-    speed = 0;
-
-  if(speed > 100.0)
-    speed = 100.0;
-
-  return (int) ( (speed / 100.0) * 255.0 );
+  DDRB |= (RF | RR);
+  DDRD |= (LF | LR);
+  TCNT0 = 0;
+  OCR0A = 0;
+  OCR0B = 0;
+  TIMSK0 |= 0b110; // OCIEA = 1 OCIEB = 1
+  TCCR0B = 0b00000011;
+  /* Our motor set up is:  
+   *    A1IN - Pin 5, PD5, OC0B
+   *    A2IN - Pin 6, PD6, OC0A
+   *    B1IN - Pin 10, PB2, OC1B
+   *    B2In - pIN 11, PB3, OC2A //Change to pin 9 oc1A
+   */
+   TCNT1 = 0;
+   OCR1A = 0;
+   OCR1B = 0;
+   TIMSK1 |= 0b110; // OCIEA = 1 OCIEB = 1
+   TCCR1B = 0b00000011;
 }
-
-// Move Alex forward "dist" cm at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// move forward at half speed.
-// Specifying a distance of 0 means Alex will
-// continue moving forward indefinitely.
-void forward(float dist, float speed)
-{
-  if(dist > 0)
-    deltaDist = dist;
-  else
-    deltaDist = 9999999;
-
-  newDist = forwardDist + deltaDist;
-  
-  dir = FORWARD;
-  int val = pwmVal(speed);
-
-  // For now we will ignore dist and move
-  // forward indefinitely. We will fix this
-  // in Week 9.
-
-  // LF = Left forward pin, LR = Left reverse pin
-  // RF = Right forward pin, RR = Right reverse pin
-  // This will be replaced later with bare-metal code.
-  
-  analogWrite(LF, val);
-  analogWrite(RF, val);
-  analogWrite(LR,0);
-  analogWrite(RR, 0);
-}
-
-// Reverse Alex "dist" cm at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// reverse at half speed.
-// Specifying a distance of 0 means Alex will
-// continue reversing indefinitely.
-void reverse(float dist, float speed)
-{
-  if(dist > 0)
-    deltaDist = dist;
-  else
-    deltaDist = 9999999;
-
-  newDist = reverseDist + deltaDist;
-  dir = BACKWARD;
-  int val = pwmVal(speed);
-
-  // For now we will ignore dist and 
-  // reverse indefinitely. We will fix this
-  // in Week 9.
-
-  // LF = Left forward pin, LR = Left reverse pin
-  // RF = Right forward pin, RR = Right reverse pin
-  // This will be replaced later with bare-metal code.
-  analogWrite(LR, val);
-  analogWrite(RR, val);
-  analogWrite(LF, 0);
-  analogWrite(RF, 0);
-}
-
-// Turn Alex left "ang" degrees at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// turn left at half speed.
-// Specifying an angle of 0 degrees will cause Alex to
-// turn left indefinitely.
-void left(float ang, float speed)
-{
-  dir = LEFT;
-  int val = pwmVal(speed);
-
-  // For now we will ignore ang. We will fix this in Week 9.
-  // We will also replace this code with bare-metal later.
-  // To turn left we reverse the left wheel and move
-  // the right wheel forward.
-  analogWrite(LR, val);
-  analogWrite(RF, val);
-  analogWrite(LF, 0);
-  analogWrite(RR, 0);
-}
-
-// Turn Alex right "ang" degrees at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// turn left at half speed.
-// Specifying an angle of 0 degrees will cause Alex to
-// turn right indefinitely.
-void right(float ang, float speed)
-{
-  dir = RIGHT;
-  int val = pwmVal(speed);
-
-  // For now we will ignore ang. We will fix this in Week 9.
-  // We will also replace this code with bare-metal later.
-  // To turn right we reverse the right wheel and move
-  // the left wheel forward.
-  analogWrite(RR, val);
-  analogWrite(LF, val);
-  analogWrite(LR, 0);
-  analogWrite(RF, 0);
-}
-
-// Stop Alex. To replace with bare-metal code later.
-void stop()
-{
-  dir = STOP;
-  analogWrite(LF, 0);
-  analogWrite(LR, 0);
-  analogWrite(RF, 0);
-  analogWrite(RR, 0);
-}
-
-/*
- * Alex's setup and run codes
- * 
- */
 
 // Clears all our counters
 void clearCounters()
